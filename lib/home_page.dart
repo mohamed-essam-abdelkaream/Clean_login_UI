0f27879224e8b2e9cee7e72c3675bdeaa5c41d32
DELL <mohamed.essam.abdelkaream@gmail.com>
2022-04-29 19:37:57 +0200

    clean login UI

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static String tag = 'home-page';
+
+  @override
+  Widget build(BuildContext context) {
+    final alucard = Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CircleAvatar(
+          radius: 72.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/mohamed.jpg'),
+        ),
+      ),
+    );
+
+    final welcome = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Welcome mohamed',
+        style: TextStyle(fontSize: 28.0, color: Colors.white),
+      ),
+    );
+
+    final lorem = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'create an account or log in to Clean Login UI with Hero Animation in Flutter  -'
+        ' A simple,fun & creative way to capture,'
+        ' edit & share photos, '
+        'videos & messages with friends & family',
+        style: TextStyle(fontSize: 16.0, color: Colors.white),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.blue,
+          Colors.lightBlueAccent,
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[alucard, welcome, lorem],
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
